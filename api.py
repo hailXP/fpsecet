ff3369d1e72c0f0e0b43d8ce5f322441bb6499cf
Hail <jyealc@gmail.com>
2024-11-08 13:14:43 +0800

    No sort

--- api.py
+++ api.py
@@ -56,25 +56,33 @@ def lich():
             eval_list.append(round(eval_score, 2))
         move_list.append(res['pv'][0].uci())
 
+    # Get side to move ('w' or 'b')
     side_to_move = fen.split()[1]
 
+    # Combine evals and moves into a list of tuples
     entries = list(zip(eval_list, move_list))
 
+    # Function to convert evals to numerical values for sorting
     def eval_to_sort_value(eval_str, side_to_move):
         if isinstance(eval_str, str) and eval_str.startswith('M'):
             mate_in = int(eval_str[1:])
             if side_to_move == 'w':
+                # For White to move, positive mate_in is good
                 return -float('inf') + mate_in if mate_in > 0 else float('inf') + mate_in
             else:
+                # For Black to move, negative mate_in is good
                 return -float('inf') - mate_in if mate_in < 0 else float('inf') - mate_in
         else:
             return eval_str
 
+    # Sort entries based on side to move
     entries.sort(key=lambda x: eval_to_sort_value(x[0], side_to_move), reverse=(side_to_move == 'w'))
 
+    # Unpack sorted entries back into eval_list and move_list
     eval_list_sorted = [x[0] for x in entries]
     move_list_sorted = [x[1] for x in entries]
 
+    # Remove evaluations with absolute value greater than 100
     filtered_evals = []
     filtered_moves = []
     for eval_val, move in zip(eval_list_sorted, move_list_sorted):
